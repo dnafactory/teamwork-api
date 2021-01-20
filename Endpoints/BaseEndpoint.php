da05bf6e10d916dfb17ab09c85571d63305c95c6
Domenico Ragusa <domenico.ragusa@dnafactory.it>
2021-01-20 17:37:28 +0100

    add missing functionality to BaseEndpoint

--- Endpoints/BaseEndpoint.php
+++ Endpoints/BaseEndpoint.php
@@ -3,20 +3,24 @@
 namespace DNAFactory\Teamwork\Endpoints;
 
 use DNAFactory\Teamwork\Exceptions\InvalidReferenceException;
-use DNAFactory\Teamwork\Factories\BaseFactory;
+use DNAFactory\Teamwork\Exceptions\NoDataExtractedException;
+use DNAFactory\Teamwork\Models\BaseModel;
 use DNAFactory\Teamwork\RawEndpoints\Proxy;
 use DNAFactory\Teamwork\Support\RequestBuilder;
-use DNAFactory\Teamwork\Support\RequestBuilderFactory;
 
 abstract class BaseEndpoint
 {
-    const TYPE_NAME = null;
+    const REF_TYPE_NAME = null;
+    const ARRAY_PATH_FOR_ENTRIES = [
+        'getById' => null,
+        'getAll' => null
+    ];
 
     protected Router $router;
     protected Proxy $rawEndpoint;
-    protected RequestBuilderFactory $requestBuilderFactory;
     protected array $cache = [];
     protected array $instancesById = [];
+    protected int $pageSize = 50;
 
     /**
      * BaseEndpoint constructor.
@@ -39,9 +43,16 @@ abstract class BaseEndpoint
         return $this->instancesById[$id];
     }
 
-    public function all()
+    public function makeRequest()
     {
         return new RequestBuilder($this);
+    }
+
+    public function allCached(): \Generator
+    {
+        foreach (array_keys($this->cache) as $id) {
+            yield $id => $this->getById($id);
+        }
     }
 
     public function loadRawEntries(array $entries)
@@ -52,10 +63,16 @@ abstract class BaseEndpoint
         }
     }
 
+    /**
+     * @param array $reference
+     * @return BaseModel|mixed
+     * @throws InvalidReferenceException
+     * @throws \DNAFactory\Teamwork\Exceptions\EndpointNotRegisteredException
+     */
     public function retriveReference(array $reference)
     {
         $type = $reference['type'] ?? null;
-        if ($type == static::TYPE_NAME) {
+        if ($type == static::REF_TYPE_NAME) {
             return $this->getByReference($reference);
         }
         return $this->router->retriveReference($reference);
@@ -64,34 +81,69 @@ abstract class BaseEndpoint
     public function getRawById($id)
     {
         if (!isset($this->cache[$id])) {
-            $this->cache[$id] = $this->rawEndpoint->getById($id);
+            $this->preload([$id]);
         }
         return $this->cache[$id];
     }
 
-    public function executeRequest(array $request): array
+    public function executeRequest(array $request)
     {
-        $pageSize = 50;
-        $pageNumber = 20; // it will be overwritten
-        for ($page = 0; $page < $pageNumber && !$hasMore; $i++) {
-            $rawData = $this->rawEndpoint->$method();
-            $pagination = $rawData['meta']['page'] ?? [];
-            $pageNumber = $pagination['count'] ?? 0;
-            $hasMore = $pagination['hasMore'] ?? false;
+        $results = $this->executeRawRequest($request);
+        foreach ($results as $result) {
+            $id = $result['id'] ?? null;
+            if (!isset($id)) {
+                continue;
+            }
+            yield $id => $this->makeOne($id);
         }
     }
 
-    protected function executeRequest2(string $method, array $params)
+    public function executeRawRequest(array $request)
     {
-        $rawData = $this->rawEndpoint->();
+        [$skip, $limit, $params] = $this->requestParams($request);
+        $unlimited = is_null($limit);
+        $method = 'getAll';
+        $n = 0;
+        while ($hasMore && ($unlimited || $n < $limit)) {
+            echo "\$rawResponse = \$this->executeRawSingleRequest($method, $params);\n";
+            $rawEntries = $this->extractData($method, $rawResponse ?? []);
+            $this->loadRawEntries($rawEntries);
+            if ($skip > $n) {
+                $rawEntries = array_slice($rawEntries, $skip - $n);
+            }
+            yield from $rawEntries;
+            $n += count($rawEntries);
+
+            $params = $this->nextPage($rawResponse, $params);
+            $hasMore = !is_null($params);
+        }
+    }
+
+    public function executeRawSingleRequest(string $method, array $params)
+    {
+        /** @var array $rawResponse */
+        $rawResponse = $this->rawEndpoint->$method(...$params);
+        $this->extractIncluded($rawResponse);
+        return $rawResponse;
     }
 
     protected function extractIncluded(array $rawData)
     {
         $included = $rawData['included'] ?? [];
         foreach ($included as $type => $entries) {
+            echo "loading ".count($entries)." $type\n";
             $this->router->loadEntries($type, $entries);
         }
+    }
+
+    protected function extractData(string $method, array $rawData)
+    {
+        $path = static::ARRAY_PATH_FOR_ENTRIES[$method] ?? null;
+        $entries = $rawData[$path] ?? null;
+        if (!isset($path, $entries)) {
+            throw new NoDataExtractedException();
+        }
+        return $entries;
     }
 
     protected function getByReference($reference)
@@ -109,10 +161,34 @@ abstract class BaseEndpoint
         $cached = array_keys($this->cache);
         $missing = array_diff($wanted, $cached);
 
-        $this->all()
-            ->filterBy('id', 'in', $missing)
+        $this->makeRequest()
+            ->filterBy(['id', 'in', $missing])
             ->toArray();
     }
 
-    protected abstract function makeOne(int $id);
+    public function requestParams(array $request)
+    {
+        $skip = $request['startAt'] ?? 0;
+        $limit = $request['endAt'] ?? null;
+        $params = ['page' => (int)floor($skip / $this->pageSize) + 1];
+        if (isset($request['relationships'])) {
+            $params['includes'] = implode(',', $request['relationships']);
+        }
+        if (isset($request['filter'])) {
+            $params['filter'] = json_encode($request['filter']);
+        }
+        return [$skip, $limit, $params];
+    }
+
+    public function nextPage(array $rawResponse, array $params): ?array
+    {
+        $hasMore = $rawResponse['meta']['page']['hasMore'] ?? false;
+        if (!$hasMore) {
+            return null;
+        }
+        $params['page'] += 1;
+        return $params;
+    }
+
+    protected abstract function makeOne(int $id): BaseModel;
 }
